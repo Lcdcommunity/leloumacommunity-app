ab2e82a029fcaba1154f6757e9ea34821b4cc307
donikodigital <doniko.digital@gmail.com>
2026-09-01 11:02:26 +0200

    correction erreur compilation

--- backend/src/modules/events/events.module.ts
+++ backend/src/modules/events/events.module.ts
@@ -3,10 +3,11 @@ import { Module } from '@nestjs/common';
 import { EventsController } from './events.controller';
 import { EventsService } from './events.service';
 import { PrismaModule } from '../../prisma/prisma.module';
-import { NotificationsModule } from '../notifications/notifications.module'; // 🔥 AJOUT CHIRURGICAL
+import { NotificationsModule } from '../notifications/notifications.module';
+import { MailModule } from '../../common/services/mail.module'; // 🔥 AJOUT : rend MailService injectable dans EventsService
 
 @Module({
-  imports: [PrismaModule, NotificationsModule], // 🔥 INJECTION ICI
+  imports: [PrismaModule, NotificationsModule, MailModule], // 🔥 AJOUT ICI
   controllers: [EventsController],
   providers: [EventsService],
   exports: [EventsService],
